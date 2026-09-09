72b5137691db1da9d306c6b6850162fc6e77efc0
bigd1ckrandy104-design <bigd1ckrandy104@gmail.com>
2026-09-08 21:04:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, ChannelType } = require('discord.js');
 const express = require('express');
 const app = express();
